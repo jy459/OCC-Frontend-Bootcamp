6bb4772e7ceb82e58c10be7d5fb0dd8b0c0864f8
jiayi <livv239@gmail.com>
2023-12-04 22:16:42 -0800

    Added JavaScript files

---
+++ 5-Javascript/app.js
@@ -0,0 +1,48 @@
+
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+
+// This is how we comment a multi line of codes 
+
+// Statements: JavaScript code is composed of statements that perform specific actions. Each statement typically ends with a semicolon (;), although it is not always required.
+
+// Comments: You can add comments in JavaScript to provide explanations or make notes within the code. There are two types of comments: single-line comments, starting with //, and multi-line comments, enclosed between /* and */.
+
+// Case sensitivity: JavaScript is case-sensitive, meaning that uppercase and lowercase letters are treated differently. For example, "variable" and "Variable" would be considered as two separate variables.
+
+// Whitespace: JavaScript ignores spaces, tabs, and line breaks (collectively called whitespace) that are not necessary for code execution. However, whitespace can enhance code readability.
+
+// Blocks: Code blocks in JavaScript are enclosed in curly braces ({}) and are used to group statements together. Blocks are often used in control structures like loops and conditional statements.
+
+// Variables// String 
+let name = "John"
+console.log("This is the name of the user:", name);
+
+var myfavoriteGame = "Animal Crossing";
+console.log("This is my favorite game:", myfavoriteGame);
+
+if (true) {
+    let myfavoriteGame = "cafe latte";
+    var mySecondFavDrink = "Tea";
+
+}
+
+// Number 
+let age = 25;
+console.log("The type of age:", typeof age);
+
+// Boolean - True or false
+let isStudent = false;
+let isTeacher = true;
+console.log("The type of isStudent:", typeof isStudent);
+console.log("The type of isTeacher:", typeof isTeacher);
+
+
+// Null- Intentional absense of data
+let girlFriend = null;
+console.log("The type of girlfriend:", typeof girlFriend);
+
+let city;
+console.log("The type of city", typeof city);
+console.log("The value of city", city);
